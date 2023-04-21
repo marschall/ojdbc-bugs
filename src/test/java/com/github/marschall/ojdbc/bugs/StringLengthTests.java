cfccaadfaab7e3b1a188fbd84d7082118fe3ae88
Philippe Marschall <philippe.marschall@gmail.com>
2023-04-21 16:08:06 +0200

    encoding tests

--- src/test/java/com/github/marschall/ojdbc/bugs/StringLengthTests.java
+++ src/test/java/com/github/marschall/ojdbc/bugs/StringLengthTests.java
@@ -21,6 +21,43 @@ class StringLengthTests {
   private DataSource dataSource;
 
   @Test
+  void dataLength() throws SQLException {
+    try (Connection connection = this.dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("""
+             SELECT data_length, char_col_decl_length, char_length, char_used
+               FROM all_tab_cols
+              WHERE data_type = 'VARCHAR2'
+            """)) {
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        while (resultSet.next()) {
+          int dataLength = resultSet.getInt("data_length");
+          int charColDeclLength = resultSet.getInt("char_col_decl_length");
+          int charLength = resultSet.getInt("char_length");
+          String charUsed = resultSet.getString("char_used");
+        }
+      }
+    }
+  }
+
+  @Test
+  void encoding() throws SQLException {
+    String output;
+    try (Connection connection = this.dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("""
+            SELECT value
+              FROM nls_database_parameters
+             WHERE parameter = 'NLS_CHARACTERSET'
+            """)) {
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        assertTrue(resultSet.next());
+        output = resultSet.getString(1);
+        assertFalse(resultSet.next());
+      }
+    }
+    assertEquals("AL32UTF8", output);
+  }
+
+  @Test
   void asciiString() throws SQLException {
     String input = "0123456789";
     assertEquals(10, input.length());
